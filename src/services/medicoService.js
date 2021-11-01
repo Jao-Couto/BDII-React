eb8163eca639665a3947f1f4ac2f48593ef472e5
RafaelN9 <rafael.n.correia@unesp.br>
2021-11-01 16:40:13 -0300

    Service com async await

--- src/services/medicoService.js
+++ src/services/medicoService.js
@@ -1,58 +1,56 @@
-import axios from "axios"
+import axios from "axios";
 
-class MedicoService{
-    
-
-    async cadastrarMedico(data){
-        
-        return axios({
-            url: "http://localhost:5000/medicos/cadastrar",
-            method: "POST",
-            timeout: 5000,
-            data: data,
-            header:{
-                Accept: 'application/json'
-            }
-        }).then((response)=>{
-            return Promise.resolve(response)
-        }).catch((error)=>{
-            return Promise.reject(error)
-        })
+class MedicoService {
+  async cadastrarMedico(data) {
+    try {
+      const cadastro = await axios({
+        url: "http://localhost:5000/medicos/cadastrar",
+        method: "POST",
+        timeout: 5000,
+        data: data,
+        header: {
+          Accept: "application/json",
+        },
+      });
+      return cadastro;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async loginMedico(data){
-        
-        return axios({
-            url: "http://localhost:5000/login",
-            method: "POST",
-            timeout: 5000,
-            data: data,
-            headers:{
-                "Access-Control-Allow-Origin": "*"
-            }
-        }).then((response)=>{
-            return Promise.resolve(response)
-        }).catch((error)=>{
-            return Promise.reject(error)
-        })
+  async loginMedico(data) {
+    try {
+      const login = await axios({
+        url: "http://localhost:5000/login",
+        method: "POST",
+        timeout: 5000,
+        data: data,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      return login;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async listarMedicos(){
-        return axios({
-            url: "http://localhost:5000/medicos/listar",
-            method: "GET",
-            timeout: 5000,
-            header:{
-                Accept: 'application/json'
-            }
-        }).then((response)=>{
-            return Promise.resolve(response)
-        }).catch((error)=>{
-            return Promise.reject(error)
-        })
+  async listarMedicos() {
+    try {
+      const listarMed = await axios({
+        url: "http://localhost:5000/medicos/listar",
+        method: "GET",
+        timeout: 5000,
+        header: {
+          Accept: "application/json",
+        },
+      });
+      return listarMed;
+    } catch (error) {
+      throw error;
     }
-
+  }
 }
 
-const medicoService = new MedicoService()
-export default medicoService
+const medicoService = new MedicoService();
+export default medicoService;
