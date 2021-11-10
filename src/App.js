17fb2a3429aafa81b9a7cbdc65558bc5e9e8c706
luan.sabino <luanphy.y@gmail.com>
2021-11-09 21:34:37 -0300

    Deletado variavel sem uso

--- src/App.js
+++ src/App.js
@@ -42,18 +42,6 @@ function App() {
     );
   }
 
-  const fakeAuth = {
-    isAuthenticated: false,
-    signin(cb) {
-      fakeAuth.isAuthenticated = true;
-      setTimeout(cb, 100); // fake async
-    },
-    signout(cb) {
-      fakeAuth.isAuthenticated = false;
-      setTimeout(cb, 100);
-    },
-  };
-
   return (
     <div className="flex flex-col items-center h-screen">
       <Router>
