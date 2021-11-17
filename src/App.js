437dece2da366befac2340493bd91fdb3043464f
luan.sabino <luanphy.y@gmail.com>
2021-11-17 14:57:54 -0300

    fix: Rota principal redireciona para login

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
           ) : (
               <Redirect
                 to={{
-                  pathname: "/",
+                  pathname: "/login",
                 }}
               />
             )
