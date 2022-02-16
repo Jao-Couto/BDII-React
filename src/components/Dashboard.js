59d84fb2db4d82f8d5791b1bd62c83093645b827
Jao-Couto <73503848+Jao-Couto@users.noreply.github.com>
2022-02-16 14:54:37 -0300

    nome total

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -18,7 +18,7 @@ export default function Dashboard(props) {
     const [totalAtendimentos, setTotalAtendimentos] = useState(0);
     const [totalSalarios, setTotalSalarios] = useState(0);
 
-    useEffect(()=>{
+    useEffect(() => {
         dashboardService.dadosDosRemedios().then(result => setGraphData(result.data));
         dashboardService.dadosDosPlanosDeSaude().then(result => setGraphData2(result.data));
         dashboardService.dadosDasUrgencias().then(result => setGraphData3(result.data));
@@ -26,60 +26,60 @@ export default function Dashboard(props) {
         dashboardService.totalPacientes().then(result => setTotalPacientes(result));
         dashboardService.totalAtendimentosConcluidos().then(result => setTotalAtendimentos(result));
         dashboardService.totalGastosSalario().then(result => setTotalSalarios(result));
-    },[])
+    }, [])
 
 
-    function cardData(str, data, sizeFont){
-        if(str)
-            return(
-            <div className="w-full h-1/5 border border-b border-t-0 p-1">
-                <p className="text-base font-bold h-1/3 text-left">{str}</p>
-                <div className={`${sizeFont ? sizeFont : 'text-6xl'} font-bold h-2/3 text-center justify-center items-center flex truncate`}>{data}</div>
-            </div>
+    function cardData(str, data, sizeFont) {
+        if (str)
+            return (
+                <div className="w-full h-1/5 border border-b border-t-0 p-1">
+                    <p className="text-base font-bold h-1/3 text-left">{str}</p>
+                    <div className={`${sizeFont ? sizeFont : 'text-6xl'} font-bold h-2/3 text-center justify-center items-center flex truncate`}>{data}</div>
+                </div>
             )
-    
-        return(
+
+        return (
             <div className={"w-full h-1/5 bg-gray-200"}></div>
-            )
-    
+        )
+
     }
 
-    function drawGraphBar(data, dataKey, title){
-        return(
+    function drawGraphBar(data, dataKey, title) {
+        return (
             <div className="w-1/2">
                 <ResponsiveContainer width="100%" height={250}>
-                <BarChart width={730} height={250} data={data}>
-                    <XAxis dataKey="nome" />
-                    <YAxis />
-                    <Tooltip />
-                    <Bar dataKey={dataKey} fill="#8884d8" />
-                </BarChart>
+                    <BarChart width={730} height={250} data={data}>
+                        <XAxis dataKey="nome" />
+                        <YAxis />
+                        <Tooltip />
+                        <Bar dataKey={dataKey} fill="#8884d8" />
+                    </BarChart>
                 </ResponsiveContainer>
                 <p className="text-center text-lg font-bold">{title}</p>
             </div>
         )
     }
 
-    function drawGraphRadar(data){
-        let maxLimitRadar = (Math.max(...data.map((a)=> a.total_usado)));
-        return(
+    function drawGraphRadar(data) {
+        let maxLimitRadar = (Math.max(...data.map((a) => a.total)));
+        return (
             <div className="w-1/2 flex items-center justify-center flex-col">
                 <ResponsiveContainer width="100%" height={250}>
-                <RadarChart outerRadius={90} width={500} height={250} data={data}>
-                    <PolarGrid />
-                    <PolarAngleAxis dataKey="nome" />
-                    <PolarRadiusAxis angle={50} domain={[0, maxLimitRadar]}/>
-                    <Radar name="Tipos de Urgencias" dataKey="total_usado" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-                    <Legend />
-                </RadarChart>
+                    <RadarChart outerRadius={90} width={500} height={250} data={data}>
+                        <PolarGrid />
+                        <PolarAngleAxis dataKey="nome" />
+                        <PolarRadiusAxis angle={50} domain={[0, maxLimitRadar]} />
+                        <Radar name="Tipos de Urgencias" dataKey="total" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                        <Legend />
+                    </RadarChart>
                 </ResponsiveContainer>
                 <p className="text-center text-lg font-bold">Proporção de Urgencias Totais</p>
             </div>
         )
     }
 
-    if(graphData !== undefined && graphData2 !== undefined && graphData3 !== undefined && graphData4 !== undefined)
-        return(
+    if (graphData !== undefined && graphData2 !== undefined && graphData3 !== undefined && graphData4 !== undefined)
+        return (
             <div className="flex w-full h-full relative">
                 <div className="h-12 absolute top-5 left-64 flex">
                     <DropdownMenu
@@ -88,14 +88,14 @@ export default function Dashboard(props) {
                         icon={<FaUsers size="24" />}
                         styles="min-w-navbar-btn"
                         options={[
-                        { name: "Atendentes", route: "/usuarios/atendentes", icon: <FaUserAlt size="18" /> },
-                        { name: "Médicos", route: "/usuarios/medicos", icon: <FaUserMd size="18" /> },
-                        { name: "Paciente", route: "/usuarios/pacientes", icon: <FaUserInjured size="18" /> }
+                            { name: "Atendentes", route: "/usuarios/atendentes", icon: <FaUserAlt size="18" /> },
+                            { name: "Médicos", route: "/usuarios/medicos", icon: <FaUserMd size="18" /> },
+                            { name: "Paciente", route: "/usuarios/pacientes", icon: <FaUserInjured size="18" /> }
                         ]}
                     />
                     <Link to="/historicoAtendimento" style={{ textDecoration: "none" }}>
                         <Button name="Histórico de Atendimentos" icon={<MdHistory size="24" />} id="historicoAtendimento" styles="min-w-navbar-btn" />
-                  </Link>
+                    </Link>
                 </div>
                 <aside className="h-full w-64 bg-white overflow-auto">
                     {cardData('Esse é o total de pacientes que temos cadastrados', totalPacientes)}
@@ -113,5 +113,5 @@ export default function Dashboard(props) {
             </div>
         );
 
-    return(<div className="animate-spin"><AiOutlineLoading3Quarters size={48}/></div>)
+    return (<div className="animate-spin"><AiOutlineLoading3Quarters size={48} /></div>)
 }
